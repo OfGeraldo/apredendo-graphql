383a994683a2ee528da680818b242af898227dee
OfGeraldo <geraldojunioroficial@gmail.com>
2021-01-27 00:54:54 -0300

    Adicionando CORS na API

--- src/server.js
+++ src/server.js
@@ -3,7 +3,24 @@ const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
 const schema = require('./api/schema')
 
+var cors = require('cors')
+
 const app = express()
+
+//app.use(cors())
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+  }
+app.use(cors(corsOptions));
+
 app.use('/api', graphqlHTTP({
     schema,
     graphiql: true
